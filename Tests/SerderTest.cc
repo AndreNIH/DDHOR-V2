445a8e1cb55b4991814cf78ea519c77a445fb237
AndreNIH <31828626+AndreNIH@users.noreply.github.com>
2022-01-07 13:36:31 -0800

    Added tests for Deserializer factory method and modified CMakeLists files

--- Tests/SerderTest.cc
+++ Tests/SerderTest.cc
@@ -1,7 +1,25 @@
 #include <gtest/gtest.h>
-#include <spdlog/spdlog.h>
-// Demonstrate some basic assertions.
-TEST(DDHOR, SerializerCreation) {
-  
+#include <gmock/gmock.h>
+#include "../src/Bot/Deserializer/Deserialization.h"
+#include "../src/Bot/Deserializer/V2.h"
+#include "../src/Bot/Deserializer/Legacy.h"
+
+using ::testing::WhenDynamicCastTo;
+using ::testing::Ne;
+using ::testing::Eq;
+
+TEST(DDHOR, DeserializerCreation) {
+  using namespace Deserializer;
+  ASSERT_THAT(BotDeserializer::create("legacy").get(),WhenDynamicCastTo<Legacy*>(Ne(nullptr)));
+  ASSERT_THAT(BotDeserializer::create("v2").get(),WhenDynamicCastTo<V2*>(Ne(nullptr)));
   
 }
+
+TEST(DDHOR, DeserializerCreationFailure) {
+  using namespace Deserializer;
+  ASSERT_THAT(BotDeserializer::create("tasbot").get(), Eq(nullptr));
+  ASSERT_THAT(BotDeserializer::create("xbot").get(), Eq(nullptr));
+  ASSERT_THAT(BotDeserializer::create("ybot").get(), Eq(nullptr));
+  ASSERT_THAT(BotDeserializer::create("zbot").get(), Eq(nullptr));
+}
+
