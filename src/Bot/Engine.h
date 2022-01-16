4e9cb91798fc56eb71e416f1d9caae7876ed2744
AndreNIH <31828626+AndreNIH@users.noreply.github.com>
2022-01-15 19:20:50 -0800

    Added BotPlayer class

--- src/Bot/Engine.h
+++ src/Bot/Engine.h
@@ -11,6 +11,7 @@
 #include <utility>
 #include <memory>
 
+#include "Commands/ClickCommands.h"
 
 /*class FakePlayer : private Deserializable, Serializable{
 private:
@@ -81,3 +82,45 @@ public:
 
 
 };*/
+
+class BotPlayer{
+    
+    std::unique_ptr<CommandBackend> _inputs = nullptr;
+    const bool _isPlayer2;
+
+    std::unique_ptr<PlayerInput>generateInputObject(){
+        auto inputObject = std::make_unique<PlayerInput>();
+        if(_isPlayer2) inputObject->setPlayer2();
+        return inputObject;
+    }
+public:
+    void insertClick(){
+        auto pushCommand = std::make_unique<DoPress>(generateInputObject());
+        _inputs->insertCommand(std::move(pushCommand));
+    }
+    void insertRelease(){
+        auto releaseCommand = std::make_unique<DoPress>(generateInputObject());
+        _inputs->insertCommand(std::move(releaseCommand));
+    }
+    
+    void rewindActions(){
+        _inputs->rewindQueue();
+    }
+    
+    void setCommandBackend(std::unique_ptr<CommandBackend>&& backend){
+        _inputs = std::move(backend);
+        if(_isPlayer2) _inputs->setPurpose(BackendPurpose::PLAYER_2);
+        else _inputs->setPurpose(BackendPurpose::PLAYER_1);
+    }
+
+    BotPlayer(bool isP2) : _isPlayer2(isP2){}
+};
+
+
+class Bot{
+    BotPlayer _player1;
+    BotPlayer _player2;
+    
+public:
+    Bot() : _player1(false), _player2(true){}
+};
