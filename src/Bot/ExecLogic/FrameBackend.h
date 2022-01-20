64844f1ab29252f7464224a50d19324e007e1483
AndreNIH <31828626+AndreNIH@users.noreply.github.com>
2022-01-19 19:30:49 -0800

    part of previous commit

--- src/Bot/ExecLogic/FrameBackend.h
+++ src/Bot/ExecLogic/FrameBackend.h
@@ -4,12 +4,13 @@
 
 
 class FrameBackend : public CommandBackend{
+private:
+    EXTEND_DESERIALIZER_ACCESS;
 public:
     void insertCommand(std::unique_ptr<BaseCommand>&& command) override;
     void rewindQueue() override;    
     void reset() override;
     void executeCommands() override;
-    
     nlohmann::json runSerializer(Serializer::BotSerializer* serializaitionObject) override;
     void runDeserializer(Deserializer::BotDeserializer* deserializaitionObject) override;
 };
