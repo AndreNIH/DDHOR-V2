94d72c64962d9f8650c9fe34c8c79590515e15f4
AndreNIH <31828626+AndreNIH@users.noreply.github.com>
2022-01-20 20:06:44 -0800

    Correct merge errors

--- src/Bot/ExecLogic/XPosBackend.h
+++ src/Bot/ExecLogic/XPosBackend.h
@@ -1,36 +1,24 @@
 #pragma once
 #include "CmdExecuterLogic.h"
 #include <vector>
-<<<<<<< HEAD
-=======
 
 #include "../Deserializer/Accessable.h"
 
->>>>>>> feature/command-migration
 class XPosBackend : public CommandBackend{
 private:
     using PCPair = std::pair<float, std::unique_ptr<BaseCommand>>;
     std::vector<PCPair> _posAndCommandsContainer;
     size_t _index;
-<<<<<<< HEAD
-public:
-    void insertCommand(std::unique_ptr<BaseCommand>&& command) override;
-    void rewindQueue() override;
-    void reset() override{}
-=======
     EXTEND_DESERIALIZER_ACCESS;
 public:
     void insertCommand(std::unique_ptr<BaseCommand>&& command) override;
     void rewindQueue() override;
     void reset() override;
->>>>>>> feature/command-migration
     void executeCommands() override;
 
     nlohmann::json runSerializer(Serializer::BotSerializer* serializaitionObject) override;
     void runDeserializer(Deserializer::BotDeserializer* deserializaitionObject) override;
 };
-<<<<<<< HEAD
-=======
 
 class CmdXPosBackend : public XPosBackend{
     EXTEND_DESERIALIZER_ACCESS;
@@ -38,4 +26,3 @@ public:
     nlohmann::json runSerializer(Serializer::BotSerializer* serializaitionObject) override;
     void runDeserializer(Deserializer::BotDeserializer* deserializaitionObject) override;
 };
->>>>>>> feature/command-migration
