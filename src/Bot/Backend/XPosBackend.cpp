913500a7bc44ec85839e9776e7fe33f93a28c115
AndreNIH <31828626+AndreNIH@users.noreply.github.com>
2022-01-21 22:24:06 -0800

    Change PlayLayer definition to gd.h's. Fix bugs in rollback and rewind logic

--- src/Bot/Backend/XPosBackend.cpp
+++ src/Bot/Backend/XPosBackend.cpp
@@ -5,29 +5,7 @@
 #include "../Deserializer/Deserialization.h"
 #include <spdlog/spdlog.h>
 
-//Dirty trick
-#pragma pack(push, 1)
-	class PlayerObject
-	{
-	public:
-		char pad_0000[32]; //0x0000
-		float xRotation; //0x0020
-		float yRotation; //0x0024
-		char pad_0028[1494]; //0x0028
-		bool orbFix; //0x05FE
-		char pad_05FF[41]; //0x05FF
-		double yVelocityVec; //0x0628
-		char pad_0630[8]; //0x0630
-		bool shipMode; //0x0638
-		char pad_0639[6]; //0x0639
-		bool isDead; //0x063F
-		char pad_0640[60]; //0x0640
-		float XPos; //0x067C
-		float YPos; //0x0680
-	}; //Size: 0x0684
-	static_assert(sizeof(PlayerObject) == 0x684);
-	static_assert(sizeof(PlayerObject) == 0x684);
-	#pragma pack(pop)
+
 
 void XPosBackend::insertCommand(std::unique_ptr<BaseCommand>&& command) {
     spdlog::info("XPosBackend::insertCommand");
@@ -38,12 +16,8 @@ void XPosBackend::insertCommand(std::unique_ptr<BaseCommand>&& command) {
 void XPosBackend::rewindQueue() {
     spdlog::info("XPosBackend::rewindQueue");
     float currentXPos= gd::PlayLayer::get()->m_pPlayer1->m_position.x;    
-    while(_index && currentXPos > _posncom[_index].first){
-        if(--_index < 0){
-            _index=0;
-            return;
-        }
-    }
+    if(_index == _posncom.size()) _index=0;
+    while(_index && currentXPos < _posncom[_index].first) _index--;
 }
 
 void XPosBackend::rollbackQueue() {
@@ -63,7 +37,7 @@ void XPosBackend::reset() {
 }
 
 void XPosBackend::executeCommands() {
-    float xpos=reinterpret_cast<PlayerObject*>(gd::PlayLayer::get()->m_pPlayer1)->XPos;
+    float xpos= gd::PlayLayer::get()->m_pPlayer1->m_position.x;
     while(_index < _posncom.size() && xpos >= _posncom[_index].first){
         spdlog::info("XPosBackend::executeCommand, current position is '{}', next command runs on", xpos, _posncom[_index].first);
         _posncom[_index].second->execute();
