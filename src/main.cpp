0c3ee3de270e94624d0533f77ef0ed4e9b8f5e6d
AndreNIH <31828626+AndreNIH@users.noreply.github.com>
2022-01-22 21:10:29 -0800

    Add spacing

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,13 @@
 #include <matdash/boilerplate.hpp>
 #include "Hooks/Game/HKPlayLayer.h"
 #include "Bot/Engine.h"
+
 void mod_main(HMODULE mod){
     AllocConsole();
     freopen_s(reinterpret_cast<FILE**>(stdout), "CONOUT$", "w", stdout);
     freopen_s(reinterpret_cast<FILE**>(stdin), "CONIN$", "r", stdin);
     gd::init();
     Hook::PlayLayer::memInit();
+
+    
 }
