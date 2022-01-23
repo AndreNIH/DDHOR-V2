3fcd36c352872d7b4a3ab4eb2a9876ce1f1d53ad
AndreNIH <31828626+AndreNIH@users.noreply.github.com>
2022-01-23 09:38:58 -0800

    re-implement deserialization for V1 ddhor macros

--- src/Bot/Deserializer/ddhor/V1.cpp
+++ src/Bot/Deserializer/ddhor/V1.cpp
@@ -5,7 +5,9 @@
 #include <utility>
 
 #include "../../Engine.h"
-
+#include "../../Backend/XPosBackend.h"
+#include "../../Commands/ClickCommands.h"
+#include <spdlog/spdlog.h>
 #pragma pack(push, 1)
 struct DDHORHeader{
     char magic[4] = {0};
@@ -22,6 +24,11 @@ T binaryRead(std::ifstream& ifs){
     return val;
 }
 
+enum class OldAction : char{
+    PUSH,
+    RELEASE
+};
+
 namespace Deserializer
 {
     void V1::deserialize(FrameBackend* object)
@@ -29,32 +36,37 @@ namespace Deserializer
         throw UnsupportedConversion{"V1 DDHOR Files do not support frame based behaviour"};
     }
     
-    void V1::deserialize(XPosBackend* object) 
+    void V1::deserialize(XPosBackend* target) 
     {
-        /*std::ifstream file(getFilename(), std::ios::binary);
+        std::ifstream file(getFilename(), std::ios::binary);
         if(!file.is_open()) throw DerError{getFilename() + " not found"};
         DDHORHeader header = binaryRead<DDHORHeader>(file);
         
-        int actionCount=0;
-        if(object->hasPlayer2Logic()){
-            constexpr size_t step = sizeof(float) + sizeof(Action);
+        const bool p2 = target->getPurpose() == BackendPurpose::PLAYER_2;
+        int actionCount= p2 ? header.p2ActionCount : header.p1ActionCount;
+        if(p2){
+            constexpr size_t step = sizeof(float) + sizeof(OldAction);
             file.seekg(step * header.p1ActionCount, std::ios::beg);
         }
         for(int i=0; i < actionCount; i++){
             float xpos    = binaryRead<float>(file);
-            Action action = binaryRead<Action>(file);
-            object->insertInput({xpos, action});
-        }*/
-        
+            OldAction oldAction = binaryRead<OldAction>(file);
+            std::unique_ptr<BaseCommand> action = nullptr;            
+            if(oldAction==OldAction::PUSH) action = std::make_unique<DoPress>(p2);
+            else action = std::make_unique<DoRelease>(p2);
+            target->_posncom.push_back({xpos, std::move(action)});
+        }        
     }
 
-    void V1::deserialize(Bot* object){
-        /*std::ifstream file(getFilename(), std::ios::binary);
+    void V1::deserialize(Bot* target){
+        std::ifstream file(getFilename(), std::ios::binary);
         if(!file.is_open()) throw DerError{getFilename() + " not found"};
         DDHORHeader header;
         file.read(reinterpret_cast<char*>(&header), sizeof(DDHORHeader));
-        object->setFPSValue(header.fps);
-        object->setMode(BotMode::kXPos);*/
+        target->setFPS(header.fps);
+        target->setBotBackend(BackendType::XPOS);
+        target->_player1._backend->runDeserializer(this);
+        target->_player2._backend->runDeserializer(this);
     }
 
     
