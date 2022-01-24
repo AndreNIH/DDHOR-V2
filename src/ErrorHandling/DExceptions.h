ca79dd9ad7c9e96f17e001f2a53bb998fd5da7a0
AndreNIH <31828626+AndreNIH@users.noreply.github.com>
2022-01-23 22:54:42 -0800

    Add FileNotFound exception class

--- src/ErrorHandling/DExceptions.h
+++ src/ErrorHandling/DExceptions.h
@@ -33,6 +33,13 @@ namespace DEX{
         explicit SerializerError(const std::string& message) : BaseEx(message.c_str()) {}
         explicit SerializerError(const char* message) : BaseEx(message) {}
     };
+
+    class FileNotFound : public std::runtime_error{
+    public:
+        using BaseEx = std::runtime_error;
+        explicit FileNotFound(const std::string& message) : BaseEx(message.c_str()) {}
+        explicit FileNotFound(const char* message) : BaseEx(message) {}
+    };
 }
 
 
