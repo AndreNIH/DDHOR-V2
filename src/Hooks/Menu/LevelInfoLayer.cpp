abdaebd6c9ead10c44399cdc2d0af921138e2def
AndreNIH <31828626+AndreNIH@users.noreply.github.com>
2022-01-27 23:42:44 -0800

    Add hook code to create ProgressButton in LevelInfoLayer

--- src/Hooks/Menu/LevelInfoLayer.cpp
+++ src/Hooks/Menu/LevelInfoLayer.cpp
@@ -1,10 +1,21 @@
 #include "LevelInfoLayer.h"
 #include <matdash.hpp>
+#include <GUI/Layer/BotMenus/MainMenu/MainMenu.h>
+#include <GUI/Widget/ProgressButton.h>
+#include <spdlog/spdlog.h>
+using namespace cocos2d;
+
+
 
 namespace Hook{
     bool LevelInfoLayer::_init(gd::LevelInfoLayer* self, gd::GJGameLevel* gamelevel){
         orig<&_init,Thiscall>(self, gamelevel);
-        //self->m_pPlayBtnMenu->removeFromParent();
+        auto button = ProgressButton::create();
+        auto pos = reinterpret_cast<CCNode*>(self->m_pPlayBtnMenu->getChildren()->objectAtIndex(0))->getPosition();
+        button->setPosition(pos);
+        button->setSizeMultiplier(1.25);
+        self->m_pPlayBtnMenu->removeAllChildren();
+        self->m_pPlayBtnMenu->addChild(button);
         return true;
     }
 
